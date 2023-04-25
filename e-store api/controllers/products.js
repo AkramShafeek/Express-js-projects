fd2d6dfad7afd1a1c96320ce8b791b5a75a93843
Akram <akramshafeek70@gmail.com>
2023-04-25 14:24:29 +0530

    Added sort, field selection, few filters and limit in query

--- e-store api/controllers/products.js
+++ e-store api/controllers/products.js
@@ -1,10 +1,41 @@
+const Products = require('../database/models/Products');
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('test error')
-    res.status(200).json({ msg: 'products testing route' })
+    const products = await Products.find({ name: 'vase table' });
+    res.status(200).json({ products, nbHits: products.length });
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: 'products route' })
+    const { name, company, featured, sort, fields, limit } = req.query;
+    const queryObject = {};
+
+    if (featured)
+        queryObject.featured = (featured === 'true' ? true : false);
+    if (company)
+        queryObject.company = company;
+    if (name)
+        queryObject.name = { $regex: name, $options: 'i' };
+
+    let result = Products.find(queryObject);
+
+    if(sort){
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    }
+    else{
+        result = result.sort('-createdAt');
+    }
+    if(fields){
+        const selectList = fields.split(',').join(' ');
+        result = result.select(selectList);
+    }
+    if(limit){
+        result = result.limit(limit);
+    }
+
+    const products = await result;
+
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 module.exports = {
